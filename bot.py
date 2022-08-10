0ddff0f0222ecc170dad821793e87621d25d0eb6
Kajanan <110709649+Kaja-97@users.noreply.github.com>
2022-08-10 11:45:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,11 +14,12 @@ import chromedriver_binary
 import telebot
 from telebot import types
 from dotenv import load_dotenv
+
+import selenium 
+import datetime
+
+
 config = load_dotenv(".env")
-
-
-
-
 
 path="chromedriver.exe"
 options=webdriver.ChromeOptions()
@@ -29,7 +30,24 @@ options.add_argument("--disable-dev-sh-usage")
 
 API_KEY=os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
-
+############################################### news paper###################################
+@bot.message_handler(commands=['Paper'])
+def greet(message):
+    today=datetime.datetime.now().strftime('%Y-%m-%d')
+    chat_id=message.chat.id
+    pagelist=[]
+#     paperlist=[]
+    for i in range(1,11):
+        ur='https://epaper.virakesari.lk/newspaper/Daily/main/{}#page-{}'.format(today,i)
+#         pagelist.append(ur)
+    for i,ii in enumerate(pagelist,start=1):
+        options=webdriver.ChromeOptions()
+        options.headless = True
+        driver =webdriver.Chrome(executable_path=r"C:\Users\kajan\Desktop\Python\Web Scraping\chromedriver",options=options)
+        driver.get(url=ii)
+        soup=BeautifulSoup(driver.page_source,'html')
+        paperpg=soup.find('img',id='pageImage')['src']
+        bot.send_photo(chat_id, paperpg, protect_content=True ,disable_notification=True)
 
 ########### telegram bot'''#########################################
 @bot.message_handler(commands=['A'])
